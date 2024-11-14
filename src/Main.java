9dc44892300152fbe1d2d5fe5f09905d2230c5c2
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2024-11-14 19:16:19 +0100

    Programa base

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int total = 0;
-        ArrayList<String> estudiants = new ArrayList<>();
-        int cont = 0;
 
-        while (true) {
-            String command = scanner.next();
-            if (command.equalsIgnoreCase("C")) {
-                total = scanner.nextInt();
-            } else if (command.equalsIgnoreCase("I")) {
-                String name = scanner.next();
-                if (cont < total) {
-                    estudiants.add(name);
-                    cont++;
-                } else {
-                    System.out.println("KO " + name);
-                }
-            } else if (command.equalsIgnoreCase("B")) {
-                String name = scanner.next();
-                if (estudiants.contains(name)) {
-                    estudiants.remove(name);
-                    cont--;
-                }
-            } else if (command.equalsIgnoreCase("A")) {
-                total = scanner.nextInt();
-            } else if (command.equalsIgnoreCase("N") || command.equalsIgnoreCase("S") || command.equalsIgnoreCase("F")) {
-                System.out.println(cont);
-                if (command.equalsIgnoreCase("F")) {
-                    break;
-                }
-            }
-        }
     }
 }
