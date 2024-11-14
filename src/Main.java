904be10316fd576e05e4f860653e23e9ddb2a221
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2024-11-14 19:39:35 +0100

    commit de branch

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        //si yo hago un cambio aqui
     }
 }
