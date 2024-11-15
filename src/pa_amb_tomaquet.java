85d79517077e6b8d7a3644961c604b3846b128fc
Gerard Jalón <gerard.jalon@gracia.lasalle.cat>
2024-11-15 17:58:35 +0100

    pa_amb_tomaquet.java

---
+++ src/pa_amb_tomaquet.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String primeraparaula = input.nextLine();
+        String segonaparaula = input.nextLine();
+        System.out.println(primeraparaula+ " amb "+ segonaparaula);
+    }
+}
