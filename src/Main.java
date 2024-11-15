5322cb450e5bdb972c81655d0496bffc9cf2b5ae
Gerard Jalón <gerard.jalon@gracia.lasalle.cat>
2024-11-15 18:24:51 +0100

    EJERCICIOS

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //si yo hago un cambio aqui
+        //Plantilla
     }
 }
